21599c8e02f6c757c5291ff20827c7d1bd2833ad
Gillian Hesse <41570430+MrsHesse@users.noreply.github.com>
2022-10-23 08:12:59 +0000

    only all to drag to adjacent cells and include cell swap after the drag ends.

--- candy.js
+++ candy.js
@@ -7,13 +7,13 @@ var boardArr = []
 
 initialiseBoard();
 
-function initialiseBoard(){
+function initialiseBoard() {
   const boardElem = document.getElementById("board");
 
-  for( let r=0; r<rows; r++){
+  for (let r = 0; r < rows; r++) {
     // create a new empty row
     var boardRow = []
-    for( let c=0; c<cols; c++){
+    for (let c = 0; c < cols; c++) {
       // create a new div for cell-back
       const newcellback = document.createElement("div");
       //console.log("boardElem  ", boardElem)
@@ -25,21 +25,21 @@ function initialiseBoard(){
 
       // pick a cell color for this cell
       const cellcolor = getColor();
-      
+
       // set up the data for cell-back
       //newcellback.dataset.row = r;
       //newcellback.dataset.col = c;
       //newcellback.dataset.color = cellcolor;
 
-      
-      
+
+
       // add as a child to the board element
       boardElem.appendChild(newcellback);
 
 
-      
+
       // create a new div for cell and set its color
-      const newcell= document.createElement("div");
+      const newcell = document.createElement("div");
       newcell.classList.add(cellcolor);
 
       // set up the data for cell-back
@@ -49,7 +49,7 @@ function initialiseBoard(){
 
       // make the newcell draggable
       //newcell.setAttribute("draggable", false);
-      
+
       // set up event listeners for this cell
       newcell.addEventListener("mouseover", overDrag);
       newcell.addEventListener("mouseout", overDragEnd);
@@ -65,12 +65,12 @@ function initialiseBoard(){
       newcell.classList.add("cell");
 
       newcellback.appendChild(newcell);
-    }  
+    }
     boardArr.push(boardRow);
   }
 }
 
-function showDetails(e){
+function showDetails(e) {
   var cellback = e.target;
 
   document.getElementById("details-row").innerHTML = cellback.dataset.row;
@@ -79,10 +79,10 @@ function showDetails(e){
 
   document.getElementById("details-color").innerHTML = cellback.dataset.color;
 
-    
+
 }
 
-function clearDetails(e){
+function clearDetails(e) {
   var cellback = e.target;
 
   document.getElementById("details-row").innerHTML = "";
@@ -94,13 +94,13 @@ function clearDetails(e){
 
 function getColor() {
 
-    // get random index value
-    const rIndex = Math.floor(Math.random() * colors.length);
+  // get random index value
+  const rIndex = Math.floor(Math.random() * colors.length);
 
-    // get random item
-    const item = colors[rIndex];
+  // get random item
+  const item = colors[rIndex];
 
-    return item;
+  return item;
 }
 
 
@@ -108,22 +108,22 @@ function getColor() {
 /* Dragging variables and functions   */
 /* ********************************** */
 
-var startcell=null;
-var endcell=null;
+var startcell = null;
+var endcell = null;
 
 // click on a cell and start the drag
-function startDrag(e){
-  if (startcell){
-    startcell.classList.remove("dragstart");  
+function startDrag(e) {
+  if (startcell) {
+    startcell.classList.remove("dragstart");
   }
-  if (endcell){
-    endcell.classList.remove("dragstart");  
-    endcell=null;
+  if (endcell) {
+    endcell.classList.remove("dragstart");
+    endcell = null;
   }
-  
+
   startcell = e.target;
   startcell.classList.add("dragstart");
-  console.log("start", startcell);
+  //console.log("start", startcell);
 
 
 
@@ -139,35 +139,72 @@ function startDrag(e){
 
 // make a cell more transparent when moving over it
 // and dragging has started
-function overDrag(e){
-  if (startcell){
-    e.target.classList.add("dragover");  
+function overDrag(e) {
+  const overcell = e.target;
+
+  if (startcell) {
+    if (isValidSwap(startcell, overcell)) {
+      overcell.classList.add("dragover");
+    }
   }
 }
 
 // set cell to normal opacity when the mouse leaves it
-function overDragEnd(e){
-  e.target.classList.remove("dragover");  
+function overDragEnd(e) {
+  e.target.classList.remove("dragover");
 }
 
 
-function endDrag(e){
+function endDrag(e) {
   e.preventDefault();
   endcell = e.target;
 
-  
-  console.log("end", endcell);
-
-  document.getElementById("details-row").innerHTML = endcell.dataset.row;
-
-  document.getElementById("details-col").innerHTML = endcell.dataset.col;
-
-  document.getElementById("details-color").innerHTML = "";
-
-  document.getElementById("details-status").innerHTML = "end drag";
+  if (isValidSwap(startcell, endcell))
+  {
+    // only carry out swap if it is a valid move.
+    swapCells(startcell, endcell);
+  }
+  //console.log("end", endcell);
+  //document.getElementById("details-row").innerHTML = endcell.dataset.row;
+  //document.getElementById("details-col").innerHTML = endcell.dataset.col;
+  //document.getElementById("details-color").innerHTML = "";
+  //document.getElementById("details-status").innerHTML = "end drag";
 
   // clear the dragging event
   startcell.classList.remove("dragstart");
   startcell = null;
   endcell.classList.remove("dragover");
+}
+
+// return true if the two cell elements are adjacent
+function isValidSwap(cell1, cell2) {
+
+  var hmove = Math.abs(cell2.dataset.row - cell1.dataset.row);
+  var vmove = Math.abs(cell2.dataset.col - cell1.dataset.col);
+
+  var validhmove = (hmove==1)&&(vmove==0);
+  var validvmove = (vmove==1)&&(hmove==0);
+
+  return (validhmove || validvmove );
+
+}
+
+// swap the two cells
+// just need to change the color data and associated class of
+// the cells
+function swapCells(cell1, cell2){
+  var color1 = cell1.dataset.color;
+  var color2 = cell2.dataset.color;
+
+  // replace color of cell1 with color2
+  cell1.dataset.color = color2;
+  cell1.classList.remove(color1);
+  cell1.classList.add(color2);
+
+  
+  // replace color of cell2 with color1
+  cell2.dataset.color = color1;
+  cell2.classList.remove(color2);
+  cell2.classList.add(color1);
+  
 }
